9740d8a21a1ca1e3ced78d0a53317859f3abd5b9
Arthur Carabott <arthurc@gmail.com>
2018-04-07 20:47:11 +0100

    support linux chromium

--- ChromeConsole.py
+++ ChromeConsole.py
@@ -94,8 +94,16 @@ def get_chrome_process():
   def make_chrome_path(basename):
     return os.path.join(os.path.dirname(get_chrome_path()), basename)
 
-  paths_to_check = ([make_chrome_path(basename) for basename in ['chrome', 'google-chrome']]
-                    if sublime.platform() == "linux" else [get_chrome_path()])
+  if sublime.platform() == "linux":
+    is_chromium = os.path.basename(get_chrome_path()) == "chromium-browser"
+
+    if is_chromium:
+      paths_to_check = [get_chrome_path()]
+    else:
+      basenames = ['chrome', 'google-chrome']
+      paths_to_check = [make_chrome_path(basename) for basename in basenames]
+  else:
+    paths_to_check = [get_chrome_path()]
 
   for process in psutil.process_iter(attrs=['exe', 'status']):
     path_matches = 'exe' in process.info and process.info['exe'] in paths_to_check
